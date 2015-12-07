49215bd71077df2709837731db49673790a777cf
Jonathan Neal <jonathantneal@hotmail.com>
2015-12-07 11:32:08 -0500

    1.1.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-short-data', function () {
 	return function (css) {
 		css.walkRules(function (rule) {
-			rule.selector = rule.selector.replace(/\[:([^\]])/, '[data-$1');
+			rule.selector = rule.selector.replace(/\[-([^\]])/, '[data-$1');
 		});
 	};
 });
