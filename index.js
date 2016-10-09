ce8f7c5e7b586607a7e1a70f81153ee07007ca81
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-10-09 22:59:51 +0300

    replace all occurencies

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-short-data', function () {
 	return function (css) {
 		css.walkRules(function (rule) {
-			rule.selector = rule.selector.replace(/\[-(?=[^\]])/, '[data-');
+			rule.selector = rule.selector.replace(/\[-(?=[^\]])/g, '[data-');
 		});
 	};
 });
